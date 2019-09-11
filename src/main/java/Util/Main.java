708f7b99b35fc5f359c3d98c30da25c67c7f5ee9
Rohit Jain <veena@Veenas-MacBook-Pro.local>
2019-09-11 19:42:03 +0530

    first commit

---
+++ src/main/java/Util/Main.java
@@ -0,0 +1,15 @@
+package Util;
+
+import java.io.File;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		FileReader file = new FileReader();
+		file.getFileFromResources("player-list.json");
+		
+	}
+
+}
---
+++ src/main/java/model/PlayerProbabilityRun.java
@@ -0,0 +1,78 @@
+package model;
+
+public class PlayerProbabilityRun {
+
+	private Integer dot;
+	private Integer one;
+	private Integer two;
+	private Integer three;
+	private Integer four;
+	private Integer five;
+	private Integer six;
+	private Integer wicket;
+
+	public Integer getDot() {
+		return dot;
+	}
+
+	public void setDot(Integer dot) {
+		this.dot = dot;
+	}
+
+	public Integer getOne() {
+		return one;
+	}
+
+	public void setOne(Integer one) {
+		this.one = one;
+	}
+
+	public Integer getTwo() {
+		return two;
+	}
+
+	public void setTwo(Integer two) {
+		this.two = two;
+	}
+
+	public Integer getThree() {
+		return three;
+	}
+
+	public void setThree(Integer three) {
+		this.three = three;
+	}
+
+	public Integer getFour() {
+		return four;
+	}
+
+	public void setFour(Integer four) {
+		this.four = four;
+	}
+
+	public Integer getFive() {
+		return five;
+	}
+
+	public void setFive(Integer five) {
+		this.five = five;
+	}
+
+	public Integer getSix() {
+		return six;
+	}
+
+	public void setSix(Integer six) {
+		this.six = six;
+	}
+
+	public Integer getWicket() {
+		return wicket;
+	}
+
+	public void setWicket(Integer wicket) {
+		this.wicket = wicket;
+	}
+
+}
