93b5b421ddbd2e0088c05e6407a28642c501bcb5
Rohit Jain <veena@Veenas-MacBook-Pro.local>
2019-09-13 19:34:41 +0530

    update

--- src/main/java/pojo/Commentry.java
+++ src/main/java/pojo/Commentry.java
@@ -2,9 +2,65 @@ package pojo;
 
 public class Commentry {
 
-	private String bowler;
+//	private String bowler;
 	private String batsman;
-	private Long runScore;
-	
+	private Integer run;
+	private boolean isOut;
+	private Integer bowlNumber;
+	private boolean isOver;
+
+	private Commentry(String bats, Integer run, Integer bowl) {
+		this.batsman = bats;
+		this.run = run;
+		this.bowlNumber = bowl;
+		this.isOut = false;
+	}
+
+	private Commentry(String bats, Integer run, Integer bowl, Boolean flag) {
+		this.batsman = bats;
+		this.run = run;
+		this.bowlNumber = bowl;
+		this.isOut = flag;
+	}
+
+	public String getBatsman() {
+		return batsman;
+	}
+
+	public void setBatsman(String batsman) {
+		this.batsman = batsman;
+	}
+
+	public Integer getRun() {
+		return run;
+	}
+
+	public void setRun(Integer run) {
+		this.run = run;
+	}
+
+	public boolean isOut() {
+		return isOut;
+	}
+
+	public void setOut(boolean isOut) {
+		this.isOut = isOut;
+	}
+
+	public Integer getBowl() {
+		return bowlNumber;
+	}
+
+	public void setBowl(Integer bowl) {
+		this.bowlNumber = bowl;
+	}
+
+	public boolean isOver() {
+		return isOver;
+	}
+
+	public void setOver(boolean isOver) {
+		this.isOver = isOver;
+	}
 
 }
--- src/main/java/pojo/Inning.java
+++ src/main/java/pojo/Inning.java
@@ -12,4 +12,53 @@ public class Inning {
 	private Long wicketFallen;
 	private Boolean isWinner;
 
+	
+	public Map<Integer, Player> getPlayer() {
+		return player;
+	}
+
+	public void setPlayer(Map<Integer, Player> player) {
+		this.player = player;
+	}
+
+	public List<Commentry> getCommentry() {
+		return commentry;
+	}
+
+	public void setCommentry(List<Commentry> commentry) {
+		this.commentry = commentry;
+	}
+
+	public Long getBowlNumber() {
+		return bowlNumber;
+	}
+
+	public void setBowlNumber(Long bowlNumber) {
+		this.bowlNumber = bowlNumber;
+	}
+
+	public Long getRemainRun() {
+		return remainRun;
+	}
+
+	public void setRemainRun(Long remainRun) {
+		this.remainRun = remainRun;
+	}
+
+	public Long getWicketFallen() {
+		return wicketFallen;
+	}
+
+	public void setWicketFallen(Long wicketFallen) {
+		this.wicketFallen = wicketFallen;
+	}
+
+	public Boolean getIsWinner() {
+		return isWinner;
+	}
+
+	public void setIsWinner(Boolean isWinner) {
+		this.isWinner = isWinner;
+	}
+
 }
